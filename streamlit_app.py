326dec80164532b8e3d7796aebbcf33dc796b2b2
Laien <laienwu0112@gmail.com>
2021-09-28 09:32:24 +0200

    zezaeaz

--- streamlit_app.py
+++ streamlit_app.py
@@ -157,7 +157,7 @@ def main():
     filename = file_selector('./data/')
     error_flag = 0
     # df = pd.read_csv(filename, encoding="ISO-8859-1")
-    df = pd.read_csv(filename, encoding="utf_16_be")
+    df = pd.read_csv(filename, engin="python")
     st.write(df.columns)
     clf = get_model(URL + '/models/clf_model.pickle')
 
